40bca8a18cde31837fcc3bd7c6611135d7611525
cyan-byte <62404444+cyan-byte@users.noreply.github.com>
2023-10-12 07:39:34 -0400

    added front-end deployment link to readme

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 
 // Routes
-app.use("/api/products", productRoutes);
+app.use("https://modern-e-commerce-store.onrender.com/api/products", productRoutes);
 
 const PORT = process.env.PORT || 3001;
 
